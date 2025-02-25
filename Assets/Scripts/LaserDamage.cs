c9259c1663ee3eb3f93107fa4d13288a7c3ba17f
BrianPalchiazaca <Brian.PalchizacaNeira@quinnipiac.edu>
2025-02-25 17:04:48 -0500

    Health system and some small addition to the UI

--- Assets/Scripts/LaserDamage.cs
+++ Assets/Scripts/LaserDamage.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class LaserDamage : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+
+    //brian's addition for UI testing
+    public TEMP_Player_Health Phealth;
     
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.gameObject == player)
         {
             Debug.Log("OW!");
+
+            //Brian's addition for UI testing
+            Phealth.Damaged();
         }
     }
 }
