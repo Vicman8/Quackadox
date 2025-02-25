653c6154f33a0a5ae52db2f7fbba74f711bc9e5b
aleenagraveline <124313468+aleenagraveline@users.noreply.github.com>
2025-02-24 22:01:30 -0500

    very very basic portal

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -3,21 +3,19 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-   /*private GameObject[] forwardPortals;
-    private GameObject[] backwardPortals;*/
+    [SerializeField] private GameObject nextPortal;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        /*forwardPortals = GameObject.FindGameObjectsWithTag("Forward");
-        backwardPortals = GameObject.FindGameObjectsWithTag("Backward");*/
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider == player)
+        if (collider.gameObject == player)
         {
-            //player.transform.position = new Vector3()
+            player.transform.position = new Vector3(nextPortal.transform.position.x + 1.5f, nextPortal.transform.position.y, nextPortal.transform.position.z);
         }
     }
 }
