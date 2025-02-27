07c82d04cac8c7d267a716588ea591dbf3d8df5d
aleenagraveline <124313468+aleenagraveline@users.noreply.github.com>
2025-02-26 22:05:08 -0500

    portal transports between scenes

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
     [SerializeField] private PlayerMovement player;
     [SerializeField] private GameObject nextPortal;
+    [SerializeField] private string nextScene;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject == player.gameObject)
         {
-            if(player.GetHorizontal() >= 0)
+            if(player.GetHorizontal() >= 0 && nextPortal != null)
             {
                 player.transform.position = new Vector3(nextPortal.transform.position.x + 1.5f, nextPortal.transform.position.y, nextPortal.transform.position.z);
             }
-            else
+            else if(nextPortal != null)
             {
                 player.transform.position = new Vector3(nextPortal.transform.position.x - 1.5f, nextPortal.transform.position.y, nextPortal.transform.position.z);
+            }
+            else if(nextScene != null)
+            {
+                SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
             }
         }
     }
