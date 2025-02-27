6a65c3443609bca8a67470bdebbacfc4420e7aee
aleenagraveline <124313468+aleenagraveline@users.noreply.github.com>
2025-02-26 21:41:31 -0500

    Update Portal.cs

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,12 +4,6 @@ public class Portal : MonoBehaviour
 {
     [SerializeField] private PlayerMovement player;
     [SerializeField] private GameObject nextPortal;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
