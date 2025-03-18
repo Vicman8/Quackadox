9313d3fdcf980d7a27b8d6cb3bb0d21bc2ea16f9
BrianPalchiazaca <Brian.PalchizacaNeira@quinnipiac.edu>
2025-03-18 10:41:09 -0400

    Enemy WIP

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -7,6 +9,8 @@ public class Enemy : MonoBehaviour
     private Rigidbody2D rb;
     private Transform currentPoint;
     public float speed;
+
+    public Transform PlayerPoint;
 
     //public EnemyAttack EnemyA;
 
@@ -23,6 +27,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
+        /*
         Vector2 point = currentPoint.position - transform.position;
         if (currentPoint == PointB.transform)
         {
@@ -44,7 +49,43 @@ public class Enemy : MonoBehaviour
         {
             currentPoint = PointB.transform;
         }
+        */
+        //Guard();
+        FollowPlayer();
 
 
     }
+
+    public void FollowPlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, PlayerPoint.position, .02f);
+    }
+
+    public void Guard()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == PointB.transform)
+        {
+            rb.linearVelocity = new Vector2(speed, 0);
+            //EnemyA.Flip();
+
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(-speed, 0);
+            //EnemyA.Flip();
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointB.transform)
+        {
+            currentPoint = PointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
+        {
+            currentPoint = PointB.transform;
+        }
+    }
+
+
 }
