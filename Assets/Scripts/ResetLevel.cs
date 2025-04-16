7f05b480852200cafa7d5a59daff6584cd72eadf
Vicman8 <Victor2044usa@gmail.com>
2025-04-15 20:39:29 -0400

    Added a portal and reset level script

---
+++ Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ResetLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggered with: " + other.name); // Debug line
+        if (other.CompareTag("Player"))
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
